7396dc376ce4fa62f8a2f81d48bbbd993553931e
Jesse Duvall <jesserduvall@outlook.com>
2022-02-20 22:51:34 -0800

    revise for better functionality

--- feb2022PA/Algorithms/fronts_SLL.js
+++ feb2022PA/Algorithms/fronts_SLL.js
@@ -1,8 +1,19 @@
+class Node {
+    constructor(value){
+        this.val = value;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+
 // Add Front
 // Write a method that accepts a value and create a new node, 
 // assign it to the list head, and return a pointer to the new head node.
 
-class SLL {
     addFront(value) {
         let new_node = new Node(value);
         if(!this.head) {
@@ -13,36 +24,41 @@ class SLL {
         this.head = new_node;
         return this;
     }
-}
-
 
 // Remove Front
 // Write a method to remove the head node and return the new 
 // list head node. If the list is empty, return null.
 
-class SLL {
     removeFront() {
         if (this.head == null){
             return null;
-        } else {
-            let temp = this.head;
-            this.head = this.next;
-            delete temp;
-            return this.head;
         }
+        let temp = this.head
+        this.head = temp.next;
+        let new_head = this.head
+        return new_head;
     }
-}
 
 // Front
 // Write a method to return the value (not the node) at the head
 // of the list. If the list is empty, return null.
 
-class SLL {
     front() {
         if (this.head == null){
             return null;
         } else if (this.next == null){
-            return this.head;
+            let front = this.head
+            return front.val;
         }
     }
 }
+
+console.log("--Add new nodes to front--")
+let sll1 = new SLL();
+sll1.addFront(4).addFront(8).addFront(5).addFront(9).addFront(6)
+console.log(sll1)
+console.log("--Remove front node and return new head node--")
+sll1.removeFront()
+console.log(sll1)
+console.log("--Return VALUE of the head of the list--")
+console.log("Head value is: " + sll1.front())
