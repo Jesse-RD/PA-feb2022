00c7e6406c5ec9552ee81b82cb287811b1de2a1a
Jesse Duvall <jesserduvall@outlook.com>
2022-02-20 22:24:56 -0800

    initial commit

---
+++ feb2022PA/Algorithms/display_SLL.js
@@ -0,0 +1,43 @@
+
+class Node {
+    constructor(value){
+        this.val = value;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+
+    addFront(value){
+        let new_node = new Node(value);
+        if (this.head == null){
+            this.head = new_node;
+            return this;
+        }
+        new_node.next = this.head;
+        this.head = new_node;
+        return this;
+    }
+
+// Display
+// Create display() that returns a string containing all list values. 
+
+    display() {
+        let runner = this.head;
+        let values = "";
+        while(runner!==null){
+            values = values + " " + runner.val;
+            runner = runner.next;
+        }
+        return values;
+    }
+}
+
+let sll1 = new SLL();
+sll1.addFront(4).addFront(8).addFront(5).addFront(9).addFront(6)
+console.log(sll1)
+
+console.log(sll1.display())
